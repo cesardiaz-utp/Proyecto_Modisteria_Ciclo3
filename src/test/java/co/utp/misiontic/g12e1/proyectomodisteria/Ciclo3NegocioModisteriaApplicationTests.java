6d5cd89f0ace4ea686ca2153f5da8039a6b837fb
Francisco Lozada <erjinsei.2210@gmail.com>
2022-09-02 22:14:50 -0500

    proyecto con el framework Springboot

---
+++ src/test/java/co/utp/misiontic/g12e1/proyectomodisteria/Ciclo3NegocioModisteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.utp.misiontic.g12e1.proyectomodisteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ciclo3NegocioModisteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
