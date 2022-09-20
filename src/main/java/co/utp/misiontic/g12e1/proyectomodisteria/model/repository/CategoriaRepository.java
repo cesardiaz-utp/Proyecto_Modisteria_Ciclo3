dc926dfe4fbaca2c3abf351426d6dfce095a671c
Francisco Lozada <erjinsei.2210@gmail.com>
2022-09-20 18:59:25 -0500

    Actualizacion clases Repository

---
+++ src/main/java/co/utp/misiontic/g12e1/proyectomodisteria/model/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package co.utp.misiontic.g12e1.proyectomodisteria.model.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.utp.misiontic.g12e1.proyectomodisteria.model.entity.Categoria;
+import co.utp.misiontic.g12e1.proyectomodisteria.model.entity.Producto;
+
+public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
+    // List<Producto> findAllByCategoriaId(Long categoriaId);
+
+    // List<Producto> findAllProducto();
+}
--- src/main/java/co/utp/misiontic/g12e1/proyectomodisteria/model/repository/ProductoRepository.java
+++ src/main/java/co/utp/misiontic/g12e1/proyectomodisteria/model/repository/ProductoRepository.java
@@ -1,5 +1,16 @@
 package co.utp.misiontic.g12e1.proyectomodisteria.model.repository;
 
-public class ProductoRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.utp.misiontic.g12e1.proyectomodisteria.model.entity.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto,Long>{
+
+    // List<Producto> findAllByCategoriaIdIn(List<Long> tags);
+
+    // Optional<Producto> findAllByCategoriaId(Long categoriaId);
     
+    List<Producto> findAllByPrecio(Integer p);
 }
