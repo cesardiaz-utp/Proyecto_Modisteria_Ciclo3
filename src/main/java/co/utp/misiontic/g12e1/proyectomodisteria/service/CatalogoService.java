3f06157abcd61a78be961cbc7ec2a76276fcc9c8
Francisco Lozada <erjinsei.2210@gmail.com>
2022-09-20 19:01:10 -0500

    Servicio de Catalogo su implementacion

--- src/main/java/co/utp/misiontic/g12e1/proyectomodisteria/service/CatalogoService.java
+++ src/main/java/co/utp/misiontic/g12e1/proyectomodisteria/service/CatalogoService.java
@@ -1,5 +1,13 @@
 package co.utp.misiontic.g12e1.proyectomodisteria.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import co.utp.misiontic.g12e1.proyectomodisteria.controller.dto.ProductoDto;
+
 public interface CatalogoService {
-    
+
+    List<ProductoDto> getProductos();
+
+    List<ProductoDto> getProductosByCategoryId(Integer categoryId);
 }
--- src/main/java/co/utp/misiontic/g12e1/proyectomodisteria/service/Impl/CatalogoServiceImpl.java
+++ src/main/java/co/utp/misiontic/g12e1/proyectomodisteria/service/Impl/CatalogoServiceImpl.java
@@ -1,7 +1,40 @@
 package co.utp.misiontic.g12e1.proyectomodisteria.service.Impl;
 
-import co.utp.misiontic.g12e1.proyectomodisteria.service.CatalogoService;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import co.utp.misiontic.g12e1.proyectomodisteria.controller.dto.ProductoDto;
+import co.utp.misiontic.g12e1.proyectomodisteria.model.repository.CategoriaRepository;
+import co.utp.misiontic.g12e1.proyectomodisteria.model.repository.ProductoRepository;
+import co.utp.misiontic.g12e1.proyectomodisteria.service.CatalogoService;
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
+@Service
 public class CatalogoServiceImpl implements CatalogoService{
+
+    private final ProductoRepository productoRepository;
+    private final CategoriaRepository categoriaRepository;
+
+    @Override
+    public List<ProductoDto> getProductos() {
+        var productos = productoRepository.findAll(Sort.by("name"));
+        
+        return productos.stream()
+                .map(p->new ProductoDto(p.getIdProducto().intValue(), p.getName(), p.getPrecio()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ProductoDto> getProductosByCategoryId(Integer categoryId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
